d9ad312817b90f1b760f468ab64b2b4a14bdfcc4
kino14n <disenokinocomp@gmail.com>
2025-07-13 19:34:07 -0500

    Forzando commit de config y requirements

--- backend/config.py
+++ backend/config.py
@@ -2,15 +2,13 @@ import os
 
 class Config:
     DB_NAME = os.getenv('DB_NAME', 'b4ntlli8yhth2jvjf7ih')
-    DB_USER = os.getenv('DB_USER', 'uymzq1mb64bx8ldn')
-    DB_PASSWORD = os.getenv('DB_PASSWORD', '3gYrCOKs8XCJY0DiLNhV')
-    DB_HOST = os.getenv('DB_HOST', 'us-east.connect.psdb.cloud')
+    DB_USER = os.getenv('DB_USER', 'uymzqlmb64bx81dn')  # Corrige aquí: 'qlmb64bx81dn'
+    DB_PASSWORD = os.getenv('DB_PASSWORD', '3gYrCOKs8XCJY0DilNhV')
+    DB_HOST = os.getenv('DB_HOST', 'b4ntlli8yhth2jvjf7ih-mysql.services.clever-cloud.com')  # Host de Clever Cloud
     DB_PORT = os.getenv('DB_PORT', '3306')
 
     SQLALCHEMY_DATABASE_URI = (
-        f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-        "?charset=utf8mb4"
-        "&ssl_ca=/etc/ssl/cert.pem"
+        f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?charset=utf8mb4"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
