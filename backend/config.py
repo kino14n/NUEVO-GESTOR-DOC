128c107c34734b37e7209def97ea2815a13943f0
kino14n <disenokinocomp@gmail.com>
2025-07-14 16:59:32 -0500

    Deploy: Procfile backend.app, configuración ready para Clever Cloud

--- backend/config.py
+++ backend/config.py
@@ -2,9 +2,9 @@ import os
 
 class Config:
     DB_NAME = os.getenv('DB_NAME', 'b4ntlli8yhth2jvjf7ih')
-    DB_USER = os.getenv('DB_USER', 'uymzqlmb64bx81dn')  # Corrige aquí: 'qlmb64bx81dn'
+    DB_USER = os.getenv('DB_USER', 'uymzq1mb64bx8ldn')  # Usuario corregido
     DB_PASSWORD = os.getenv('DB_PASSWORD', '3gYrCOKs8XCJY0DilNhV')
-    DB_HOST = os.getenv('DB_HOST', 'b4ntlli8yhth2jvjf7ih-mysql.services.clever-cloud.com')  # Host de Clever Cloud
+    DB_HOST = os.getenv('DB_HOST', 'b4ntlli8yhth2jvjf7ih-mysql.services.clever-cloud.com')
     DB_PORT = os.getenv('DB_PORT', '3306')
 
     SQLALCHEMY_DATABASE_URI = (
